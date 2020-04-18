5373d24e24fd344beaf54144a213056d00f9afbe
December G <dgarnetsmith@gmail.com>
2020-04-18 00:35:54 -0600

    Implement testing in the booking and booking repository classes, almost finish last method for booking repository class

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -12,13 +12,51 @@ describe('Booking', function() {
   //create variable for date
 
   beforeEach(() => {
-    booking1 = new Booking();
-    booking2 = new Booking();
+    booking1 = new Booking(bookingTestData[0]);
+    booking2 = new Booking(bookingTestData[1]);
+     chai.spy.on(booking1, ['deleteBooking'], () => true)
+     chai.spy.on(booking2, ['deleteBooking'], () => true)
   });
 
-  it.skip('should be an instance of Booking', function() {
+  afterEach(() => {
+    chai.spy.restore(booking1);
+    chai.spy.restore(booking2);
+  });
+
+  it('should be an instance of Booking', function() {
     expect(booking1).to.be.an.instanceof(Booking);
     expect(booking2).to.be.an.instanceof(Booking);
+  });
+
+  it('should have an id', function() {
+    expect(booking1.id).to.equal("5fwrgu4i7k55hl6t8");
+    expect(booking2.id).to.equal("5fwrgu4i7k55hl6wc");
+  });
+
+  it('should have a userID', function() {
+    expect(booking1.userID).to.equal(1);
+    expect(booking2.userID).to.equal(4);
+  });
+
+  it('should have a date', function() {
+    expect(booking1.date).to.equal("2020/02/05");
+    expect(booking2.date).to.equal("2020/02/05");
+  });
+
+  it('should have a room number', function() {
+    expect(booking1.roomNumber).to.equal(12);
+    expect(booking2.roomNumber).to.equal(23);
+  });
+
+  it('should have room service charges', function() {
+    expect(booking1.roomServiceCharges).to.deep.equal([]);
+    expect(booking2.roomServiceCharges).to.deep.equal([]);
+  });
+
+  it('should successfully delete a booking', function() {
+
+    expect(booking1.deleteBooking()).to.equal(true);
+    expect(booking2.deleteBooking()).to.equal(true);
   });
 
 
